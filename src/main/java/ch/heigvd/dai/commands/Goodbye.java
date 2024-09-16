8492c80308ec79287db83f0bfb506b3f5954ec2c
Mathieu Emery <145534747+mathieuemery@users.noreply.github.com>
2024-09-16 16:31:09 +0200

    Correction de l'indentation

--- src/main/java/ch/heigvd/dai/commands/Goodbye.java
+++ src/main/java/ch/heigvd/dai/commands/Goodbye.java
@@ -3,7 +3,7 @@ import ch.heigvd.dai.Main;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 @CommandLine.Command(name = "goodbye", description = "Print a 'Goodbye World!' type of message.")
-        public class Goodbye implements Callable<Integer> {
+public class Goodbye implements Callable<Integer> {
         @CommandLine.ParentCommand protected Main parent;
         @CommandLine.Option(
                 names = {"-f", "--farewells"},
@@ -15,4 +15,4 @@ import picocli.CommandLine;
         System.out.println(farewells + " " + parent.getName() + "!");
         return 0;
         }
-        }
+}
